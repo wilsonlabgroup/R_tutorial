2b2acfbb6b546676f5936de286d251e50e92f33c
mimi31k <m.liang@mail.utoronto.ca>
2020-02-21 03:27:26 -0500

    updated session1 content to include dataTypes. Improved script content and renamed

--- session1_dataTypes.R
+++ session1_dataTypes.R
@@ -1,22 +1,79 @@
-# Understanding data types and strucutres and how to operate on those.
+# Understanding data types and structures and how to operate on those.
 
 ### R has 6 atomic data types [a set of values having pre-defined characteristics]
 # use typeof() or class() to see what kind of object is it.
-
 typeof("a") # character
 class('2') # character
 class(2.5) # numeric
 class(2L) # integer ; the L tells R to store this as an integer
 class(TRUE) # logical 
+class(1+2i) # complex 
+
 # TRUE and FALSE can be represented as T and F, but it is recommended to use the full names. 
 F <- 5
 class(F)
-typeof(1+2i) # complex 
+
+##### the number 1 represented in multiple ways!
+one.numeric <- 1
+one.integer <- 1L
+one.logical <- TRUE
+one.complex <- 1+0i
+one.charact <- "1"
+
+##### Logical operators with different data types
+one.numeric == 1 #TRUE
+one.integer == 1 #TRUE
+one.complex == 1 #TRUE
+one.logical == 1 #TRUE
+
+one.charact
+one.charact == 1 #TRUE (Depends on which language!)
+"TRUE" == TRUE   #TRUE (Depends on which language!)
+"1" == TRUE      #FALSE !!!
+
+##### More on logical comparisons with strings:
+1== 1 #TRUE
+"1"==" 1"   #FALSE -- whitespace matters!
+"A" == "a"  #FALSE -- case matters!
+
+### Greater than/ less than is determined by alphabetical order
+"B" > "A"  #TRUE
+"A" > "a"  #TRUE
+"A" > "b"  #FALSE
+"A" > "1"  #TRUE
+"A" >  1   #TRUE (Depends on which language!)
+
+### Data types can affect results!
+ 2  >  12  #FALSE
+"2" > "12" #TRUE
+ 2  > "12" #TRUE
+"2" >  12  #TRUE
+
+### Convert between data types: coercion
+x <- "1"
+x + 1
+class(x)
+class(as.numeric(x)) ### as.xxxx coerces data types
+
+as.numeric(x) + 1
+
+as.character(1)
+as.logical(1)
+as.complex(1)
+as.integer(1)
+
+### Not all coercions are possible:
+as.numeric("one") #warning!
+as.logical("1")   #NA
+
+### Multiple coercions can be chained together:
+as.logical(as.numeric("1"))
+
 
 ##### R has many data structures
 ### Atomic vectors [a sequence of values]
 # The most basic data structures in r. 
-vector() # default vector is logical 
+logical(5) # default vector is logical 
 character(5) # Create an empty character vector
 numeric(10)
 
@@ -43,41 +100,57 @@ tail(large)
 z <- c(z, "Today")
 z
 
+# Vectors can be "named"
+x
+names(x) <- c("a","b","c","d","e")
+x
+
 # Sequence of numbers
 x <- c(1:5) # / 1:5
 seq(5)
 ?seq
 seq(1, 10, by = 2) # seq(10,2,-3)
 
-# Missing data and special values.
-x <- c(1, NA, 4, 5, NA)
-class(x)
-is.na(x) # is.na() indicates if the elements of the vectors represent missing data
-anyNA(x) # if the vector contains any missing data
-
--1/0 # Inf: infinity
-0/0 # NaN: Not a Number 
-
-## Conversion between modes of storage: coertion
+# Mixing data types in vectors:
+z
+class(z) #character
 x
+class(x) #numeric
+
+mix <- c(x, z)
+mix
+class(mix) #When different data types are combined into the same vector, they are forced to the "Largest common denominator". 
+
+#Vectors can be coerced just like single values
+x
+as.character(x)
 class(as.character(x))
 
 y <- c("5", "6", "7")
 as.numeric(y)
 
-mix <- c(x, z)
+#Coercion can cause problems with inappropriate values
 mix
-class(mix) # When different data types are combined into the same vector, they are forced to the same type. 
-mix
-# The source of many surprises!! Always be aware of data types. 
+convert.mix <- as.numeric(mix)
+convert.mix
 
-## Vectors can have names. 
-names(y) 
-names(y) <- c("a", "b", "c","d")
-y
+# Missing data and special values.
+NA   # missing value
+NULL # nothing there!
+1/0  # Inf: infinity
+0/0  # NaN: Not a Number 
+
+is.null(NULL) #TRUE
+is.na(NA) #TRUE
+is.na(NaN) #TRUE
+is.finite(Inf) #FALSE
+
+convert.mix
+is.na(convert.mix) # is.na() indicates if the elements of the vectors represent missing data
+anyNA(convert.mix) # if the vector contains any missing data
 
 ### Factors. 
-# Factors are important in R. Factors look like characters but are used to represent categorical information. 
+# Factors are important in R. Factors look like character vectors but are used to represent categorical information. 
 colors <- c("red", "blue", "blue", "purple", "red")
 class(colors)
 colors <- as.factor(colors)
@@ -91,9 +164,11 @@ str(colors)
 # Levels of factors can be represented by numbers. Be very careful when converting factors and  numberic values. 
 some_numbers <- c(1, 2, 6, 9, 10, 2, 6)
 class(some_numbers)
+as.factor(some_numbers)
+as.numeric(as.factor(some_numbers))
 as.numeric(as.character(as.factor(some_numbers)))
 
-
+####### BREAK ###########
 
 ### Matrix
 # An atomic vectors with dimensions: only one data type is allowed. 
