e94781deac373aa08eacaed56e801be7625e38b6
Huayun Hou <huayun@Huayuns-MacBook-Pro.local>
2020-02-20 23:27:45 -0500

    Update session2 data frame

--- session2_dataFrames.R
+++ session2_dataFrames.R
@@ -1,33 +1,82 @@
 # Manipulating data frames
-# Read in a data frame from file. 
-# Write a comma-deliminated file by "File -> New File -> Text File". 
-dat <- read.table(file = "example1.csv", header = TRUE, sep = ",")
+# Load R data "iris"
+data(iris)
+head(iris)
+
+# Quickly examine the object 
+class(iris)
+head(iris)
+tail(iris)
+dim(iris)
+summary(iris)
+names(iris)
+str(iris)
+
+# Let's make a smaller data frame
+mydf <- rbind(iris[c(1:3, 51:53, 101:103),])
+
 
 # Subsetting a data frame is very similar with subsetting a matrix. 
-typeof(dat[2,])
-class(dat[,"height"])
-# You can use the $ to subset by names
-dat$color # characters are by default stored as factors.
-dat <- read.table(file = "example.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
-dat$color <- as.character(dat$color)
-class(t(dat))
+# Subsetting by indices
+mydf[,2]
+mydf[3, c(2,5)]
+mydf[-4, -c(3:10)]
+
+# Subsetting by name
+# How to explain the outputs of the following commands?
+mydf[,"Species"] 
+mydf["Species"] 
+mydf[["Species"]]
+mydf$Species
+
+# data frames are just lists!
+
+# characters are by default stored as factors.
+
 # Subsetting with logical operations
-dat[dat$color == "red", ]
+mydf[mydf$Species == "versicolor", ]
+mydf[mydf$Species == "versicolor", c("Sepal.Length", "Petal.Length", "Species")]
+# Another way to obtain the same result?
+
+
+
+
+
+# common mistake!
+vars <- c("Sepal.Length", "Petal.Length")
+mydf$vars
+
+
+
+
 
 # Subset using the function subset
-subset(dat, color == "red") # columns can be referred to (by name) as variables in the function call
+subset(mydf, Species == "versicolor") # columns can be referred to (by name) as variables in the function call
+# subset takes 3 parameters: the data frame; how to subset by row; which columns to select
+subset(mydf, Species == "versicolor", select = c(Sepal.Width, Petal.Width))
 
+##############################################################
+# Challenge 5
+##############################################################
 
 # Adding columns/rows to data frames.
-name <- c("a", "b", "c", "d")
-dat <- cbind(dat, name)
+# Create a data frame in R
 
-dat$name2 <- c("e", "f", "g", "h")
+mydf <- data.frame(genes = c("A", "B", "C", "D"),
+                   length = c(1000, 30000, 4000, 1100),
+                   target = c(TRUE, TRUE, FALSE, FALSE),
+                   stringsAsFactors = FALSE)
+# Add a row
+newrow <- c("E", "200", TRUE)
+mydf <- rbind(mydf, newrow)
 
-dat[, c("height", "weight", "name", "color")]
-### Write data frame to a file.
-write.table(dat, file = "example_modified.txt", col.names = TRUE, row.names = FALSE, quote = FALSE, sep = "\t")
+newrow <- c("E", 200, "F", "G")
+rbind(mydf, newrow)
 
+# Add a column
+expression <- c(50, 155, 6, 15, 10 )
+cbind(mydf, expression)
+# New column must have same number of rows!
 
 
 
